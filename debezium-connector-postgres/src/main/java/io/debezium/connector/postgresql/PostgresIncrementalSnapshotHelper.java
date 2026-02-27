ca61a65fffcc692576ecb058911b8f5c7f65d3b3
Joan Gomez <joan.gomezgomez@datadoghq.com>
2026-02-27 15:15:08 +0100

    Fix missing PostgresConnection import in PostgresIncrementalSnapshotHelper

--- debezium-connector-postgres/src/main/java/io/debezium/connector/postgresql/PostgresIncrementalSnapshotHelper.java
+++ debezium-connector-postgres/src/main/java/io/debezium/connector/postgresql/PostgresIncrementalSnapshotHelper.java
@@ -11,6 +11,7 @@ import java.util.OptionalLong;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.debezium.connector.postgresql.connection.PostgresConnection;
 import io.debezium.relational.TableId;
 
 /**
